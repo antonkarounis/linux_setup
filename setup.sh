0af5a23ea1775dcfedd193efc917a12d5d5cc07e
Antony D Karounis <nullfocus@gmail.com>
2020-05-05 16:20:44 -0400

    fixed copy/paste error in script

--- setup.sh
+++ setup.sh
@@ -60,9 +60,7 @@ function install_guitar {
 
 function install_npm {
     curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-    sudo apt-get install     LINE=$1
-80
-    FILE=$2-y nodejs
+    sudo apt-get install -y nodejs
 }
 
 function install_java8 {

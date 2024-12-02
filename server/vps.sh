5344cb536f0450b804b1acca381691b087ea6b7b
Antony D Karounis <nullfocus@gmail.com>
2024-12-02 18:41:07 -0500

    Update vps.sh

--- server/vps.sh
+++ server/vps.sh
@@ -5,7 +5,6 @@ sudo apt update
 sudo apt upgrade
 sudo shutdown -r now
 
-
 sudo apt install wget curl
 
 # ---- install docker ----- 
@@ -13,7 +12,7 @@ sudo apt install wget curl
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -40,6 +39,25 @@ sudo apt install nginx
 # 1. run through guide here: https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-22-04
 # 2. setup let's encrypt here: https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-20-04
 # 3. set up reverse proxy here: https://www.digitalocean.com/community/tutorials/how-to-configure-nginx-as-a-reverse-proxy-on-ubuntu-22-04
+
+sudo apt install nginx
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
+sudo ufw enable
+
+sudo mkdir -p /var/www/[domain]/html
+sudo chown -R $USER:$USER /var/www/[domain]/
+sudo nano /var/www/[domain]/html/index.html
+sudo nano /etc/nginx/sites-available/[domain]
+
+ln -s /etc/nginx/sites-available/[domain] /etc/nginx/sites-enabled/
+
+sudo nano /etc/nginx/nginx.conf
+sudo nginx -t
+sudo systemctl restart nginx
+
+sudo apt install certbot python3-certbot-nginx
+sudo certbot --nginx -d [domain]
 
 # ---- github ----
 

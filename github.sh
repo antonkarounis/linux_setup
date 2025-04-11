e7c1ee48b345c9adc47f747c0bb07084c061279e
Anton Karounis <nullfocus@gmail.com>
2025-04-10 21:23:26 -0400

    added github script

---
+++ github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt_install git
+
+ssh-keygen -t ed25519 -C "nullfocus@gmail.com"  -N "" -f ~/.ssh/github.key
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github.key
+
+git config --global user.email "nullfocus@gmail.com"
+git config --global user.name "Antony Karounis"
